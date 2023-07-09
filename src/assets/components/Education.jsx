b96dc84127e13de8793e2eb72e0b6d88f4edc3f7
Ibrahim Damilola Dhikirullahi <oloyedammy@gmail.com>
2023-07-09 05:32:25 -0700

    education

--- src/assets/components/Education.jsx
+++ src/assets/components/Education.jsx
@@ -1,30 +1,43 @@
 import React from 'react'
 
 const Education = () => {
+    const schools = [
+        {
+            title: "The Polytechnic, Ibadan",
+            cert: "Higher National Diploma in Computer Science",
+            img: "/image/Polyibadan.png",
+            year: "October, 2016 - November, 2018"
+        },
+        {
+            title: "The Polytechnic, Ibadan",
+            cert: "Higher National Diploma in Computer Science",
+            img: "/image/Polyibadan.png",
+            year: "October, 2016 - November, 2018"
+        },
+        {
+            title: "The Polytechnic, Ibadan",
+            cert: "Higher National Diploma in Computer Science",
+            img: "/image/Polyibadan.png",
+            year: "October, 2016 - November, 2018"
+        }
+    ]
   return (
     <>
         <h3>Education</h3>
         <div id="educationCon">
-            <div id="eduCard">
-                <img src="/image/Polyibadan.png" alt="" />
-                <h5>The Polytechnic, Ibadan</h5>
-                <em>Higher National Diploma in Computer Science</em>
-                <em>October, 2016 - November, 2018</em>
+        {
+            schools.map((sch, index) => (
+
+            <div key={index} id="eduCard">
+                <img className='img-fluid' width={"80px"} src={sch.img} alt="" />
+                <h5>{sch.title}</h5>
+                <strong>{sch.cert}</strong>
+                <em>{sch.year}</em>
             </div>
 
-            <div id="eduCard">
-                <img src="/image/Polyibadan.png" alt="" />
-                <h5>The Polytechnic, Ibadan</h5>
-                <em>Higher National Diploma in Computer Science</em>
-                <em>October, 2016 - November, 2018</em>
-            </div>
+            ))
+        }
 
-            <div id="eduCard">
-                <img src="/image/Polyibadan.png" alt="" />
-                <h5>The Polytechnic, Ibadan</h5>
-                <em>Higher National Diploma in Computer Science</em>
-                <em>October, 2016 - November, 2018</em>
-            </div>
         </div>
     </>
   )
